b6a1d632ee400c96ff76625bcd5f60fc794c274a
Zac King <zac.king@and.digital>
2022-11-14 09:27:00 +0000

    Git is being very weird!!

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,25 @@
-const Controller_IPFS = require("./IPFS/controller_ipfs");
+const Controller_IPFS = require('./IPFS/controller_ipfs')
 
 async function main() {
+  const controller_ipfs = new Controller_IPFS()
 
-    const controller_ipfs = new Controller_IPFS();
+  console.log('Initialising IPFS...')
+  await controller_ipfs.initialise()
 
-    console.log("Initialising IPFS...");
-    await controller_ipfs.initialise();
-    
-    console.log("Uploading to IPFS...");
-    const cID = await controller_ipfs.upload("Hello!!!!!!!!!");
-    
-    console.log("cID =", cID);
+  console.log('Uploading to IPFS...')
+  const cID = await controller_ipfs.upload('Hello!!!!!!!!!')
 
-    console.log("Downloading from IPFS...");
-    const resumeText = await controller_ipfs.download(cID);
+  console.log('cID =', cID)
 
-    console.log("Resume Text:", resumeText);
+  console.log('Downloading from IPFS...')
+  const resumeText = await controller_ipfs.download(cID)
 
+  console.log('Resume Text:', resumeText)
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
