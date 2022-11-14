43f7d74bfa31914d0e3f86dcd0c67ed204cf7516
Zac King <zac.king@and.digital>
2022-11-14 11:19:12 +0000

    Format correction

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 const Controller_IPFS = require('./IPFS/controller_ipfs')
 
 async function main() {
+    
+    const controller_ipfs = new Controller_IPFS()
 
-  const controller_ipfs = new Controller_IPFS()
+    console.log('Initialising IPFS...')
+    await controller_ipfs.initialise()
 
-  console.log('Initialising IPFS...')
-  await controller_ipfs.initialise()
+    console.log('Uploading to IPFS...')
+    const cID = await controller_ipfs.upload('HJWChbYixcWZCQqe')
 
-  console.log('Uploading to IPFS...')
-  const cID = await controller_ipfs.upload('HJWChbYixcWZCQqe')
+    console.log('cID =', cID)
 
-  console.log('cID =', cID)
+    console.log('Downloading from IPFS...')
+    const resumeText = await controller_ipfs.download(cID)
 
-  console.log('Downloading from IPFS...')
-  const resumeText = await controller_ipfs.download(cID)
+    console.log('Resume Text:', resumeText)
 
-  console.log('Resume Text:', resumeText)
-
-  console.log("Removing from IPFS... (Note, this only asks peers to delete it eventually!)")
-  await controller_ipfs.remove(cID);
+    console.log("Removing from IPFS... (Note, this only asks peers to delete it eventually!)")
+    await controller_ipfs.remove(cID);
 
 }
 
